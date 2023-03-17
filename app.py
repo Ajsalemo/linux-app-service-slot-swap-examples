967428394cfa910bc176bea04c3642db4c6a9755
Anthony Salemo <ajssalemo@gmail.com>
2023-03-16 21:49:00 -0400

    testing to use wafc with github actions

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'linux-app-service-slot-swap-examples-ghactions'
+    return 'linux-app-service-wafc-slot-swap-examples-ghactions'
